5fc1af3fb0ae535254e3932619d35e7025bb15aa
mrakas1993 <mrakas1993@gmail.com>
2025-10-11 15:25:29 +0300

    add anim + health1

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -28,6 +28,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            anim.SetBool("jump",false); //Деактивируем прыжок
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -65,6 +66,7 @@ public class PlayerController : MonoBehaviour
         if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //Прыжок
+            anim.SetBool("jump",true); //Активируем прыжок
         }
     }
 }
