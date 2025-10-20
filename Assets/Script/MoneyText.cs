814bcb0a1b1af5ce9bba1f805ecf20839e61c4fb
mrakas1993 <mrakas1993@gmail.com>
2025-10-20 20:39:04 +0300

    Cinemamashine + coin

--- Assets/Script/MoneyText.cs
+++ Assets/Script/MoneyText.cs
@@ -1,17 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MoneyText : MonoBehaviour
 {
     public static int Coin;
-    private TextMeshPro text;
+    Text text;
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<TextMeshPro>();
+        text = GetComponent<Text>();
+       
     }
 
     // Update is called once per frame
